de506a347687de8748f7619ed507d9a137ee9e11
David Probst <david.probst@tum.de>
2021-10-04 15:54:49 +0200

    Reihenfolge is now a DataTable which drastically reduces rendering time for bigger numSeats

--- src/components/OutputViews/ReihenfolgeOutput.jsx
+++ src/components/OutputViews/ReihenfolgeOutput.jsx
@@ -1,41 +1,59 @@
 import React from "react";
 import PropTypes from "prop-types";
-import { Table, Tbody, Tr, Td } from "@chakra-ui/react";
+import { Spinner } from "@chakra-ui/react";
+
+import DataTable from "./DataTable";
 
 ReihenfolgeOutput.propTypes = {
   assignmentSequence: PropTypes.array,
 };
 
 export default function ReihenfolgeOutput({ assignmentSequence }) {
-  if (assignmentSequence == null) {
-    return (
-      <p>
-        Bei dem Verfahren Hare/Niemeyer ergibt sich keine Zugriffsreihenfolge
-      </p>
-    );
-  }
+  // TODO
+  // transform to use Memo?
+  const [data, setData] = React.useState();
 
-  let i = 0;
+
+  const columns = React.useMemo(
+    () => [
+      {
+        Header: 'Position',
+        id: 'index',
+        Cell: ({ row, flatRows }) => {
+          return flatRows.indexOf(row) + 1;
+        },
+      },
+      {
+        Header: 'Reihenfolge',
+        accessor: 'assignmentSequence.seat_goes_to', // accessor is the "key" in the data
+        Cell: ({ cell: { value } }) => {
+          if (Array.isArray(value)){
+            return ('Mehrdeutig! ' + value.join(' oder '))
+          } else {
+            return value
+          }
+        }
+      },
+
+    ],
+    []
+  )
+
+  React.useEffect(() => {
+    if (assignmentSequence != undefined) {
+      const assignmentSequenceParsed = assignmentSequence.map((partyName) => { return {'assignmentSequence': partyName}})
+      console.log(assignmentSequenceParsed)
+      setData(assignmentSequenceParsed);
+    }
+  }, [assignmentSequence]);
+
   return (
-    <Table>
-      <Tbody>
-        {assignmentSequence.map((sequenceEntry) => {
-          i++;
-          return (
-            <Tr key={`${JSON.stringify(sequenceEntry.seat_goes_to)}${i}`}>
-              <Td>{i}.</Td>
-              <Td>
-                {sequenceEntry.is_ambiguous ? (
-                    `Mehrdeutig zwischen ${sequenceEntry.seat_goes_to.join(' und ')}!` 
-                  ) : (
-                    sequenceEntry.seat_goes_to
-                  )
-                }
-              </Td>
-            </Tr>
-          );
-        })}
-      </Tbody>
-    </Table>
+    <>
+      {columns == undefined || data == undefined ? (
+        <Spinner />
+      ) : (
+        <DataTable data={data} columns={columns} />
+      )}
+    </>
   );
 }
--- src/components/OutputViews/TabellenOutput.jsx
+++ src/components/OutputViews/TabellenOutput.jsx
@@ -31,7 +31,6 @@ export default function TabellenOutput({ tableData }) {
               } else {
                 return value
               }
-              
             }
           }
         )
