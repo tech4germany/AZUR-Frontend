375c3fba34aa6ce933dae422855eacd33ef1eab5
David Probst <david.probst@tum.de>
2021-10-07 16:11:51 +0200

    AZUR Default abstracted into own component

--- src/App.js
+++ src/App.js
@@ -1,42 +1,14 @@
 import React from "react";
-import AzurInputs from "./components/Input";
-import Output from "./components/Output";
-import useAzur from "./hooks/useAzur";
+import AzurDefault from "./components/AzurDefault";
 
-import { Flex } from "@chakra-ui/react";
+import { Box } from "@chakra-ui/react";
 
 function App() {
-  const [azurInput, setAzurInput] = React.useState({data: {}, errors: {}});
-  const { data, loading, error } = useAzur(azurInput);
 
   return (
-    <Flex
-      className="App"
-      flexDirection={["column", "column", "column", "row"]}
-      height="100vh"
-    >
-      <AzurInputs
-        azurInput={azurInput}
-        setAzurInput={setAzurInput}
-        backgroundColor="gray.50"
-        height="100%"
-        overflowY="auto"
-        width="80ex" // Todo proper width layout
-        px="10"
-        py="5"
-      />
-      <Output
-        azurInput={azurInput}
-        azurResponse={data}
-        azurError={error}
-        loading={loading}
-        height="100%"
-        px="10"
-        py="5"
-        flexGrow={1}
-        overflowY="auto"
-      />
-    </Flex>
+    <Box className="App">
+      <AzurDefault />
+    </Box>
   );
 }
 
---
+++ src/components/AzurCompare.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+import { Flex } from "@chakra-ui/react";
+
+export default function AzurDefault() {
+  // const [azurCompareInput, setAzurCompareInput] = React.useState({ data: {}, errors: {} });
+
+  return (
+    <Flex
+    flexDirection={["column", "column", "column", "row"]}
+    height="100vh"
+  >
+  </Flex>
+  );
+}
