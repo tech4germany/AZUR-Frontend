a94d09709e073a8cbc1ecc09994e48084e582f0b
David Probst <david.probst@tum.de>
2021-10-14 16:23:56 +0200

    Added changedRowCount message

--- src/views/AzurCompare/Output/Output.jsx
+++ src/views/AzurCompare/Output/Output.jsx
@@ -7,25 +7,48 @@ import {
   AlertDescription,
   AlertIcon,
   AlertTitle,
+  Text,
   Box,
+  Center,
 } from "@chakra-ui/react";
 import PropTypes from "prop-types";
 import ComparisonTable from "./ComparisonTable";
 import _ from "lodash";
 
 const Output = ({ data, error, loading, ...cssprops }) => {
+  let changedRowCount = "unbekannt";
+  if (data?.table != undefined) {
+    changedRowCount = data.table.filter((row) => !row.is_identical).length;
+  }
+
   return (
     <Card {...cssprops}>
-      <Heading size="2xl">Ergebniss</Heading>
+      <Heading size="2xl">Ergebnis</Heading>
       {loading ? (
         <Spinner color="brand.orange" />
       ) : (
         <Box bg="white">
           {_.isEmpty(error) ? (
-            <ComparisonTable
-              tableData={data?.table}
-              assignmentSequence={data?.assignment_sequence}
-            />
+            <Center flexDirection="column">
+              <Box textAlign="center" p="3" px="20" fontSize="xl">
+                <Text d="inline" fontWeight="bold">
+                  {changedRowCount}
+                </Text>{" "}
+                von{" "}
+                <Text d="inline" fontWeight="bold">
+                  {data?.table.length || "unbekant"}
+                </Text>{" "}
+                Zeilen haben sich geändert,{" "}
+                <Text d="inline" fontWeight="bold">
+                  {data?.table.length - changedRowCount || "unbekant"}
+                </Text>{" "}
+                sind gleich geblieben.
+              </Box>
+              <ComparisonTable
+                tableData={data?.table}
+                assignmentSequence={data?.assignment_sequence}
+              />
+            </Center>
           ) : (
             <Alert status="error">
               <AlertIcon />
