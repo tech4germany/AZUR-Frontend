fc90ab4a035603bbcb5d9a24ab8c268042237de0
David Probst <david.probst@tum.de>
2021-10-14 11:15:26 +0200

    Added basic routing

--- src/App.js
+++ src/App.js
@@ -1,12 +1,35 @@
 import React from "react";
 import AzurDefault from "views/AzurDefault/AzurDefault";
-
-import { Box } from "@chakra-ui/react";
+import AzurCompare from "views/AzurCompare/AzurCompare";
+import { BrowserRouter, Switch, Route, Link } from "react-router-dom";
+import { Box, Flex, Button } from "@chakra-ui/react";
 
 function App() {
   return (
     <Box className="App">
-      <AzurDefault />
+      <BrowserRouter>
+        {/* NAV */}
+        <Flex justifyContent="end" mx="10">
+          <Link to="/">
+            <Button>Azur Default</Button>
+          </Link>
+          <Link to="/azur_compare">
+            <Button>Azur Compare</Button>
+          </Link>
+        </Flex>
+
+        {/* APP */}
+        <Box>
+          <Switch>
+            <Route exact={true} path="/">
+              <AzurDefault />
+            </Route>
+            <Route exact={true} path="/azur_compare">
+              <AzurCompare />
+            </Route>
+          </Switch>
+        </Box>
+      </BrowserRouter>
     </Box>
   );
 }
