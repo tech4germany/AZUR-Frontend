17d586cef070dc5759758a06758fe1a6658e9e5d
David Probst <david.probst@tum.de>
2021-10-18 18:31:08 +0200

    removed link for azur compare and nav

--- src/App.js
+++ src/App.js
@@ -1,30 +1,30 @@
 import React from "react";
 import AzurDefault from "views/AzurDefault/AzurDefault";
 import AzurCompare from "views/AzurCompare/AzurCompare";
-import { BrowserRouter, Switch, Route, Link } from "react-router-dom";
-import { Box, Flex, Button } from "@chakra-ui/react";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import { Box } from "@chakra-ui/react";
 
 function App() {
   return (
     <Box className="App">
       <BrowserRouter>
         {/* NAV */}
-        <Flex justifyContent="end" mx="10">
+        {/*         <Flex justifyContent="end" mx="10" height="5vh">
           <Link to="/">
             <Button>Azur Default</Button>
           </Link>
-          <Link to="/azur_compare">
+          <Link to="/compare">
             <Button>Azur Compare</Button>
           </Link>
-        </Flex>
+        </Flex> */}
 
         {/* APP */}
-        <Box>
+        <Box height="100vh">
           <Switch>
             <Route exact={true} path="/">
               <AzurDefault />
             </Route>
-            <Route exact={true} path="/azur_compare">
+            <Route exact={true} path="/compare">
               <AzurCompare />
             </Route>
           </Switch>
